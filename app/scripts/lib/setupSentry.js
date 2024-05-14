54bfd8e6adde4421e069ab0669ca26d173d75c00
Salim Toubal <salim.toubal@outlook.com>
2024-05-14 12:48:17 +0200

    feat: add patch

--- app/scripts/lib/setupSentry.js
+++ app/scripts/lib/setupSentry.js
@@ -340,8 +340,6 @@ export const SENTRY_BACKGROUND_STATE = {
     },
   },
   TokenRatesController: {
-    contractExchangeRates: false,
-    contractExchangeRatesByChainId: false,
     marketData: false,
   },
   TokensController: {
