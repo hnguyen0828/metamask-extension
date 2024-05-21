d67a9e78d76f9caa8eb86732b7932ceafbb2be0e
Niranjana Binoy <43930900+NiranjanaBinoy@users.noreply.github.com>
2024-05-21 17:27:44 -0400

    updating the controller pattern, renaming the env variable, updating some state files to fix circle ci failure'

--- app/scripts/metamask-controller.js
+++ app/scripts/metamask-controller.js
@@ -772,9 +772,14 @@ export default class MetamaskController extends EventEmitter {
       this.metaMetricsController.handleMetaMaskStateUpdate(update);
     });
 
+    const metaMetricsDataDeletionMessenger =
+      this.controllerMessenger.getRestricted({
+        name: 'MetaMetricsDataDeletionController',
+      });
     this.metaMetricsDataDeletionController =
       new MetaMetricsDataDeletionController({
-        initState: initState.metaMetricsDataDeletionController,
+        messenger: metaMetricsDataDeletionMessenger,
+        state: initState.metaMetricsDataDeletionController,
         metaMetricsController: this.metaMetricsController,
       });
 
@@ -2176,8 +2181,7 @@ export default class MetamaskController extends EventEmitter {
       KeyringController: this.keyringController,
       PreferencesController: this.preferencesController.store,
       MetaMetricsController: this.metaMetricsController.store,
-      MetaMetricsDataDeletionController:
-        this.metaMetricsDataDeletionController.store,
+      MetaMetricsDataDeletionController: this.metaMetricsDataDeletionController,
       AddressBookController: this.addressBookController,
       CurrencyController: this.currencyRateController,
       NetworkController: this.networkController,
@@ -2238,7 +2242,7 @@ export default class MetamaskController extends EventEmitter {
         PreferencesController: this.preferencesController.store,
         MetaMetricsController: this.metaMetricsController.store,
         MetaMetricsDataDeletionController:
-          this.metaMetricsDataDeletionController.store,
+          this.metaMetricsDataDeletionController,
         AddressBookController: this.addressBookController,
         CurrencyController: this.currencyRateController,
         AlertController: this.alertController.store,
